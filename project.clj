52ed87ac011836ae27d27ec9f130840c2d9f4f5f
Tap <visibletrap@gmail.com>
2017-05-09 13:55:48 +0800

    Add tests

---
+++ project.clj
@@ -0,0 +1,2 @@
+(defproject ninety-nine-bottles "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]])
---
+++ src/ninety_nine_bottles/core.clj
@@ -0,0 +1,7 @@
+(ns ninety-nine-bottles.core)
+
+(defn verse [ln])
+
+(defn verses [sln eln])
+
+(defn song [])
